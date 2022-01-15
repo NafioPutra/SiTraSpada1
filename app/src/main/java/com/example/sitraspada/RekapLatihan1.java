79c65bfc97a2754a9aa5a934a974b1ad5f0c5a9b
NafioPutra <pelupa576@gmail.com>
2022-01-15 21:24:04 +0700

    layout prestasi

---
+++ app/src/main/java/com/example/sitraspada/RekapLatihan1.java
@@ -0,0 +1,14 @@
+package com.example.sitraspada;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RekapLatihan1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rekap_latihan1);
+    }
+}
